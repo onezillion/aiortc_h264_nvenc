0306635a606d5ac69ce75d3008fe9bd444405d9a
Kun-Han Lee <myonezillion@gmail.com>
2026-01-02 13:38:44 +0800

    Release

--- src/aiortc/__init__.py
+++ src/aiortc/__init__.py
@@ -50,7 +50,7 @@ from .stats import (
     RTCTransportStats,
 )
 
-__version__ = "1.14.0.post3"
+__version__ = "1.14.0.post4"
 
 # Set default logging handler to avoid "No handler found" warnings.
 logging.getLogger(__name__).addHandler(logging.NullHandler())
